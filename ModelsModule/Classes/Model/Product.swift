85c1f96d7b6fa34036ff023bf0dba537e1cd8bf0
Alejandro.Castillo <alejandro.castillo@fluxit.com.ar>
2021-08-12 20:11:06 -0300

    asset added in module

--- ModelsModule/Classes/Model/Product.swift
+++ ModelsModule/Classes/Model/Product.swift
@@ -6,15 +6,18 @@
 //
 
 import Foundation
+import UIKit
 
 public struct Product {
     public let id          : Int?
     public let title       : String?
     public let price       : Double?
+    public let image       : UIImage?
     
-    public init(id: Int, title: String, price: Double) {
+    public init(id: Int, title: String, price: Double, image: UIImage) {
         self.id     = id
         self.title  = title
         self.price  = price
+        self.image  = image
     }
 }
